a8b6aa1077319dc425f0bc3078ac1079fd0bffb4
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-14 13:10:27 +0000

    With login

--- api/LoginAndGetGroupsTrigger/index.js
+++ api/LoginAndGetGroupsTrigger/index.js
@@ -1,14 +1,32 @@
 const crypto = require('crypto');
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+
+const checkLogin = (username, password) => {
+    const passwordHashed = crypto.createHash('sha256').update(password, 'utf8').digest().toString('base64');
+    const logins = JSON.parse(process.env.LOGINS);
+    for (let login of logins) {
+        if (login.username === username && login.passwordHash === passwordHashed) {
+            return login;
+        }
+    }
+    return null;
+};
 
 module.exports = async function (context, req) {
     context.log('Login and get groups processed.');
 
-    const passwordHashed = crypto.createHash('sha256').update(req.body.password, 'utf8').digest().toString('base64');
+    const login = checkLogin(req.body.username, req.body.password);
+    if (!login) {
+        context.res = {
+            status: 400,
+            body: "Pas autorisé"
+        };
+        return;
+    }
 
     context.res = {
-        body: {
-            username: req.body.username,
-            passwordHash: passwordHashed
-        }
+        body: login.sheets
     };
 }
