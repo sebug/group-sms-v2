99df10c1a197e9d02716f36a432c1df9a8d662ba
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-14 12:55:38 +0000

    Returning password hash

--- api/LoginAndGetGroupsTrigger/index.js
+++ api/LoginAndGetGroupsTrigger/index.js
@@ -1,10 +1,14 @@
+const crypto = require('crypto');
+
 module.exports = async function (context, req) {
     context.log('Login and get groups processed.');
+
+    const passwordHashed = crypto.createHash('sha256').update(req.body.password, 'utf8').digest().toString('base64');
 
     context.res = {
         body: {
             username: req.body.username,
-            password: req.body.password
+            passwordHash: passwordHashed
         }
     };
 }
