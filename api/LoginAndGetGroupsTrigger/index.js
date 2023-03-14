ab84285ce54b7ab1d72228ea260411cc69a83b0b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-14 13:31:21 +0000

    Adding get sheets

--- api/LoginAndGetGroupsTrigger/index.js
+++ api/LoginAndGetGroupsTrigger/index.js
@@ -2,6 +2,8 @@ const crypto = require('crypto');
 const fs = require('fs').promises;
 const path = require('path');
 const process = require('process');
+const os = require('os');
+const {Auth, google} = require('googleapis');
 
 const checkLogin = (username, password) => {
     const passwordHashed = crypto.createHash('sha256').update(password, 'utf8').digest().toString('base64');
@@ -14,21 +16,73 @@ const checkLogin = (username, password) => {
     return null;
 };
 
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorizeForGoogle(context) {
+    await fs.writeFile(path.join(os.tmpdir(), "keyfile.json"), process.env.SERVICE_ACCOUNT_KEY);
+
+    const auth = new Auth.GoogleAuth({
+        keyFile: path.join(os.tmpdir(), "keyfile.json"),
+        scopes: SCOPES
+    });
+
+    const client = await auth.getClient();
+
+    context.log('Client is ' + client);
+    context.log(client);
+
+    return client;
+  }
+
 module.exports = async function (context, req) {
-    context.log('Login and get groups processed.');
+    try {
+        context.log('Login and get groups processed.');
 
-    const login = checkLogin(req.body.username, req.body.password);
-    if (!login) {
-        context.res = {
-            status: 400,
-            body: "Pas autorisé"
-        };
-        return;
-    }
-
-    context.res = {
-        body: {
-            sheets: login.sheets
+        const login = checkLogin(req.body.username, req.body.password);
+        if (!login) {
+            context.res = {
+                status: 400,
+                body: "Pas autorisé"
+            };
+            return;
         }
-    };
+    
+        const client = await authorizeForGoogle(context);
+
+        const sheets = google.sheets({version: 'v4', auth: client });
+
+        context.log(process.env.SHEET_ID);
+
+        const res = await sheets.spreadsheets.get({
+            spreadsheetId: process.env.SHEET_ID
+        });
+
+        const loginSheets = login.sheets.split(',');
+
+        const allowedSheets = [];
+        for (const sheetInfo of res.data.sheets) {
+            if (loginSheets.filter(s => s === 'all' || s === sheetInfo.properties.title)) {
+                allowedSheets.push({
+                    sheetId: sheetInfo.properties.sheetId,
+                    title: sheetInfo.properties.title
+                });
+            }
+        }
+    
+        context.res = {
+            body: {
+                sheets: allowedSheets
+            }
+        };
+    } catch (err) {
+        context.log(err);
+        context.res = {
+            status: 500,
+            body: '' + err
+        };
+    }
 }
